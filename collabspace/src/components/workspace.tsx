1ff9fdd684cb8cc228fdeaf15cadc9021e03e78c
Doug Martin <doug@zoopdoop.com>
2017-12-06 13:34:10 -0500

    Added relative url to drawing tool and case table

--- collabspace/src/components/workspace.tsx
+++ collabspace/src/components/workspace.tsx
@@ -276,17 +276,22 @@ export class WorkspaceComponent extends React.Component<WorkspaceComponentProps,
     }
   }
 
+  constructRelativeUrl(filename:string) {
+    const {location} = window
+    return `${location.origin}${location.pathname}${filename}`
+  }
+
   handleAddDrawingButton() {
     const title = (prompt("Enter the title of the drawing", "Untitled Drawing") || "").trim()
     if (title.length > 0) {
-      this.windowManager.add(`${window.location.origin}/drawing-tool.html`, title)
+      this.windowManager.add(this.constructRelativeUrl("drawing-tool.html"), title)
     }
   }
 
   handleAddCaseTable = () => {
     const title = (prompt("Enter the title of the table", "Untitled Table") || "").trim()
     if (title.length > 0) {
-      this.windowManager.add(`${window.location.origin}/neo-codap.html`, title)
+      this.windowManager.add(this.constructRelativeUrl("neo-codap.html"), title)
     }
   }
 
