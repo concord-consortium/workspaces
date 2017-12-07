386c3312f27583cbcca3503efdeeb61909b0719d
Kirk Swenson <kswenson@concord.org>
2017-12-07 08:03:34 -0800

    Add comment to `typings.d.ts` with links to rationale

--- neo-codap/src/typings.d.ts
+++ neo-codap/src/typings.d.ts
@@ -1,3 +1,5 @@
+// cf. https://hackernoon.com/import-json-into-typescript-8d465beded79
+// cf. https://stackoverflow.com/a/43833423
 declare module "*.json" {
   const value: any;
   export default value;
