ebb9c976abf536bd471ab3613b9c20e6b545332f
Kirk Swenson <kswenson@concord.org>
2017-12-21 11:29:55 -0800

    Add X & Y axis menus for changing attributes on axes

--- neo-codap/src/graph/graph.tsx
+++ neo-codap/src/graph/graph.tsx
@@ -4,7 +4,8 @@ import * as d3 from 'd3';
 import sizeMe from 'react-sizeme';
 import { IAttribute } from '../data-manager/attribute';
 import { ICase, IDataSet, IDerivationSpec } from '../data-manager/data-manager';
-import { find } from 'lodash';
+import { Menu, MenuItem, Popover, Position } from '@blueprintjs/core';
+import { assign, find } from 'lodash';
 import './graph.css';
 
 interface ISizeMeSize {
@@ -17,10 +18,15 @@ interface IGraphProps {
     dataSet: IDataSet;
 }
 
-interface IGraphState {
-    graphData?: IDataSet;
+interface IGraphData {
+    dataSet?: IDataSet;
     xAttrID?: string;
     yAttrID?: string;
+}
+
+interface IGraphState extends IGraphData {
+    xMenuIsOpen: boolean;
+    yMenuIsOpen: boolean;
 }
 
 export class GraphComponent extends React.Component<IGraphProps, IGraphState> {
@@ -31,7 +37,10 @@ export class GraphComponent extends React.Component<IGraphProps, IGraphState> {
     constructor(props: IGraphProps) {
         super(props);
 
-        this.state = this.createGraphData(props.dataSet);
+        this.state = assign({}, this.createGraphData(props.dataSet), {
+                                xMenuIsOpen: false,
+                                yMenuIsOpen: false
+                            });
 
         this.attachHandlers(this.props.dataSet);
     }
@@ -55,10 +64,12 @@ export class GraphComponent extends React.Component<IGraphProps, IGraphState> {
         return found ? (found as {} as IAttribute).id : undefined;
     }
 
-    createGraphData(srcDataSet?: IDataSet): IGraphState {
+    createGraphData(srcDataSet?: IDataSet, attrs?: IGraphData): IGraphData {
         if (!srcDataSet) { return {}; }
 
-        let { xAttrID, yAttrID } = this.state ? this.state : {} as IGraphState,
+        // client-specified attributes override state attributes
+        const graphData = assign({}, this.state ? this.state : {} as IGraphData, attrs);
+        let { xAttrID, yAttrID } = graphData,
             xAttr: IAttribute, yAttr: IAttribute,
             attrIDs: string[] = [];
         if (!xAttrID) {
@@ -99,10 +110,10 @@ export class GraphComponent extends React.Component<IGraphProps, IGraphState> {
                 },
                 synchronize: true
             },
-            graphData = srcDataSet && srcDataSet.derive('graphData', derivationSpec);
+            dataSet = srcDataSet && srcDataSet.derive('graphData', derivationSpec);
 
-        this.attachHandlers(undefined, graphData);
-        return { graphData, xAttrID, yAttrID };
+        this.attachHandlers(undefined, dataSet);
+        return { dataSet, xAttrID, yAttrID };
     }
 
     attachHandlers(srcData?: IDataSet, graphData?: IDataSet) {
@@ -132,8 +143,8 @@ export class GraphComponent extends React.Component<IGraphProps, IGraphState> {
                     if (!srcData.isInTransaction) {
                         if (graphDataIncomplete &&
                             (this.srcAttributesChanged || this.srcValuesChanged)) {
-                            this.setState(this.createGraphData(srcData));
-                        }
+                                this.setState(this.createGraphData(srcData));
+                            }
                         this.srcAttributesChanged = false;
                         this.srcValuesChanged = false;
                     }
@@ -162,29 +173,111 @@ export class GraphComponent extends React.Component<IGraphProps, IGraphState> {
     componentWillReceiveProps(nextProps: IGraphProps) {
         const { dataSet } = nextProps;
         if (dataSet !== this.props.dataSet) {
-            this.detachHandlers(this.props.dataSet, this.state.graphData);
-            const newState = this.createGraphData(dataSet);
-            this.attachHandlers(dataSet, newState.graphData);
-            this.setState(newState);
+            this.detachHandlers(this.props.dataSet, this.state.dataSet);
+            const newGraphData = this.createGraphData(dataSet);
+            this.attachHandlers(dataSet, newGraphData.dataSet);
+            this.setState(newGraphData);
         }
     }
 
     componentWillUnmount() {
-        this.detachHandlers(this.props.dataSet, this.state.graphData);
+        this.detachHandlers(this.props.dataSet, this.state.dataSet);
+    }
+
+    handleSelectAttribute = (evt: React.MouseEvent<HTMLElement>) => {
+        const elt: HTMLElement = evt.target as HTMLElement,
+              classes = elt.className,
+              axisMatch = /data-axis-([xy])/.exec(classes),
+              axis = axisMatch && axisMatch[1],
+              idMatch = /data-id-([-\w]*)/.exec(classes),
+              attrID = idMatch && idMatch[1];
+        if (axis && attrID) {
+            let attrs: IGraphData = {};
+            switch (axis) {
+                case 'x':
+                    attrs.xAttrID = attrID;
+                    break;
+                case 'y':
+                default:
+                    attrs.yAttrID = attrID;
+                    break;
+            }
+            this.setState(this.createGraphData(this.props.dataSet, attrs));
+        }
+    }
+
+    renderAttributeMenu(axisLabel: string) {
+        const renderAttributeItems = () => {
+            if (!this.props.dataSet) { return null; }
+            return this.props.dataSet.attributes.map((attr) => {
+                return (
+                    <MenuItem
+                      className={`data-axis-${axisLabel} data-id-${attr.id}`}
+                      text={attr.name}
+                      key={attr.id}
+                      onClick={this.handleSelectAttribute}
+                    />
+                );
+            });
+        };
+        return (
+            <Menu>
+                {renderAttributeItems()}
+            </Menu>
+        );
+    }
+
+    renderXAxisPopover() {
+        const handlePopoverInteraction = (nextOpenState: boolean) => {
+            this.setState({ xMenuIsOpen: nextOpenState });
+        };
+        return (
+            <div className="nc-popover-container nc-x-popover-container">
+                <Popover
+                    popoverClassName="nc-popover-menu nc-x-popover-menu"
+                    content={this.renderAttributeMenu('x')}
+                    position={Position.TOP}
+                    isOpen={this.state.xMenuIsOpen}
+                    onInteraction={handlePopoverInteraction}
+                >
+                    <div/>
+                </Popover>
+            </div>
+        );
+    }
+
+    renderYAxisPopover() {
+        const handlePopoverInteraction = (nextOpenState: boolean) => {
+            this.setState({ yMenuIsOpen: nextOpenState });
+        };
+        return (
+            <div className="nc-popover-container nc-y-popover-container">
+                <Popover
+                    popoverClassName="nc-popover-menu nc-y-popover-menu"
+                    content={this.renderAttributeMenu('y')}
+                    position={Position.RIGHT}
+                    isOpen={this.state.yMenuIsOpen}
+                    onInteraction={handlePopoverInteraction}
+                >
+                    <div/>
+                </Popover>
+            </div>
+        );
     }
 
     render() {
-        if (!this.props.size.width || !this.props.size.height || !this.state.graphData) {
+        if (!this.props.size.width || !this.props.size.height || !this.state.dataSet) {
             return null;
         }
 
         const kPointRadius: number = 6;
 
         const
-            { graphData, xAttrID, yAttrID } = this.state,
-            graphCaseCount = graphData && graphData.cases.length;
-        let xAttr = graphData.attrFromID(xAttrID || ''),
-            yAttr = graphData.attrFromID(yAttrID || '');
+            { dataSet, xAttrID, yAttrID } = this.state,
+            graphCaseCount = dataSet && dataSet.cases.length;
+        if (!dataSet) { return null; }
+        let xAttr = dataSet.attrFromID(xAttrID || ''),
+            yAttr = dataSet.attrFromID(yAttrID || '');
         let xValues: number[] = xAttr ? xAttr.values as number[] : [],
             yValues: number[] = yAttr ? yAttr.values as number[] : [];
         let xMin = graphCaseCount ? Math.min(0, d3.min(xValues) || 0) : 0,
@@ -237,7 +330,8 @@ export class GraphComponent extends React.Component<IGraphProps, IGraphState> {
             .append('text')
             .attr('transform', 'translate(' + (width / 2) + ',' + (margin.bottom - 3) + ')')
             .style('text-anchor', 'middle')
-            .text(xAttr ? xAttr.name : '');
+            .text(xAttr ? xAttr.name : '')
+            .on('click', () => this.setState({ xMenuIsOpen: true }));
 
         svg.append('g')
             .attr('class', 'y axis')
@@ -247,7 +341,8 @@ export class GraphComponent extends React.Component<IGraphProps, IGraphState> {
             .attr('y', -margin.left + 20)
             .attr('x', -height / 2)
             .style('text-anchor', 'middle')
-            .text(yAttr ? yAttr.name : '');
+            .text(yAttr ? yAttr.name : '')
+            .on('click', () => this.setState({ yMenuIsOpen: true }));
 
         svg.selectAll('circle')
             .data(coordinates)
@@ -264,12 +359,9 @@ export class GraphComponent extends React.Component<IGraphProps, IGraphState> {
         // const {divWidth, divHeight} = this.props.size;
         return (
             <div className="neo-codap-graph">
-{/*
-                <div className="nc-graph-message">
-                    Graph: {graphCaseCount} of {totalCaseCount} cases are plottable!
-                </div>
-*/}
                 {node.toReact()}
+                {this.renderXAxisPopover()}
+                {this.renderYAxisPopover()}
             </div>
         );
     }
