452c8f43776f93437bc772e2aa7f183e08d006b0
Doug Martin <doug@zoopdoop.com>
2018-02-04 10:45:11 -0500

    Refactored line button rendering

--- drawing/src/components/toolbar.tsx
+++ drawing/src/components/toolbar.tsx
@@ -6,10 +6,16 @@ import { EventEmitter, Events } from "../lib/events"
 
 export const TOOLBAR_WIDTH = 48
 
-export const BLACK = "#000"
-export const RED = "#f00"
-export const BLUE = "#00f"
-export const GREEN = "#006400"
+export interface LineColor {
+  name: string
+  hex: string
+}
+export const lineColors:LineColor[] = [
+  {name: "Black", hex: "#000"},
+  {name: "Red", hex: "#f00"},
+  {name: "Green", hex: "#006400"},
+  {name: "Blue", hex: "#00f"},
+]
 
 export interface ToolbarViewProps {
   mode: DrawingMode
@@ -21,11 +27,16 @@ export interface ImageButtonData {
   imageSetItem: ImageSetItem
 }
 
-export type ToolbarModalButton = "edit" | "drawBlackLine" | "drawRedLine" | "drawBlueLine" | "drawGreenLine" | "image" | "select"
+export interface LineButtonData {
+  lineColor: LineColor
+}
+
+export type ToolbarModalButton = "edit" | "line" | "image" | "select"
 
 export interface ToolbarViewState {
   selectedButton: ToolbarModalButton
   selectedImageSetItem: ImageSetItem|null
+  selectedLineColor: LineColor
 }
 
 export class ToolbarView extends React.Component<ToolbarViewProps, ToolbarViewState> {
@@ -34,7 +45,8 @@ export class ToolbarView extends React.Component<ToolbarViewProps, ToolbarViewSt
 
     this.state = {
       selectedButton: "edit",
-      selectedImageSetItem: null
+      selectedImageSetItem: null,
+      selectedLineColor: lineColors[0]
     }
 
     this.addEventListeners()
@@ -42,37 +54,35 @@ export class ToolbarView extends React.Component<ToolbarViewProps, ToolbarViewSt
 
   addEventListeners() {
     this.props.events.listen(Events.EditModeSelected, () => this.setState({selectedButton: "edit"}))
-    this.props.events.listen(Events.LineDrawingToolSelected, (data) => {
-      switch (data.color) {
-        case BLACK:
-          this.setState({selectedButton: "drawBlackLine"})
-          break
-        case RED:
-          this.setState({selectedButton: "drawRedLine"})
-          break
-        case BLUE:
-          this.setState({selectedButton: "drawBlueLine"})
-          break
-        case GREEN:
-          this.setState({selectedButton: "drawGreenLine"})
-          break
-      }
-    })
-    this.props.events.listen(Events.SelectionToolSelected, () => this.setState({selectedButton: "select"}))
+    this.props.events.listen(Events.LineDrawingToolSelected, (data:LineButtonData) => this.setState({selectedButton: "line", selectedLineColor: data.lineColor}))
     this.props.events.listen(Events.ImageToolSelected, (data:ImageButtonData) => this.setState({selectedButton: "image", selectedImageSetItem: data.imageSetItem}))
+    this.props.events.listen(Events.SelectionToolSelected, () => this.setState({selectedButton: "select"}))
   }
 
   handleEditModeButton = () => this.props.events.emit(Events.EditModeSelected)
-  handleLineDrawingToolButton = (color:string) => () => this.props.events.emit(Events.LineDrawingToolSelected, {color: color})
+  handleLineDrawingToolButton = (lineColor:LineColor) => () => this.props.events.emit(Events.LineDrawingToolSelected, {color: lineColor.hex})
   handleSelectionToolButton = () => this.props.events.emit(Events.SelectionToolSelected)
   handleImageToolButton = (data:ImageButtonData) => this.props.events.emit(Events.ImageToolSelected, {imageSetItem: data.imageSetItem})
   handleUndoButton = () => this.props.events.emit(Events.UndoPressed)
   handleRedoButton = () => this.props.events.emit(Events.RedoPressed)
   handleDeleteButton = () => this.props.events.emit(Events.DeletePressed)
 
+  lineButtonClass(color:LineColor) {
+    const selected = "line" === this.state.selectedButton && (color === this.state.selectedLineColor)
+    return `button ${selected ? "selected" : ""}`
+  }
+
   modalButtonClass(type:ToolbarModalButton, imageSetItem?:ImageSetItem) {
     const selected = type === this.state.selectedButton && ((type !== "image") || (imageSetItem === this.state.selectedImageSetItem))
     return `button ${selected ? "selected" : ""}`
+  }
+
+  renderLineButtons() {
+    return lineColors.map((lineColor) => {
+      const selected = "line" === this.state.selectedButton && (lineColor === this.state.selectedLineColor)
+      const className = `button ${selected ? "selected" : ""}`
+      return <div className={className} title={`${lineColor.name} Line Drawing Mode`} onClick={this.handleLineDrawingToolButton(lineColor)} style={{color: lineColor.hex}}>🖉</div>
+    })
   }
 
   renderImageSetItems() {
@@ -86,10 +96,7 @@ export class ToolbarView extends React.Component<ToolbarViewProps, ToolbarViewSt
       <div className="toolbar" style={{width: TOOLBAR_WIDTH}}>
         <div className="buttons">
           <div className={this.modalButtonClass("edit")} title="Edit Mode" onClick={this.handleEditModeButton}>A</div>
-          <div className={this.modalButtonClass("drawBlackLine")} title="Black Line Drawing Mode" onClick={this.handleLineDrawingToolButton(BLACK)} style={{color: BLACK}}>🖉</div>
-          <div className={this.modalButtonClass("drawRedLine")} title="Red Line Drawing Mode" onClick={this.handleLineDrawingToolButton(RED)} style={{color: RED}}>🖉</div>
-          <div className={this.modalButtonClass("drawBlueLine")} title="Blue Line Drawing Mode" onClick={this.handleLineDrawingToolButton(BLUE)} style={{color: BLUE}}>🖉</div>
-          <div className={this.modalButtonClass("drawGreenLine")} title="Green Line Drawing Mode" onClick={this.handleLineDrawingToolButton(GREEN)} style={{color: GREEN}}>🖉</div>
+          {this.renderLineButtons()}
           {this.renderImageSetItems()}
           <div className={this.modalButtonClass("select")} title="Select" onClick={this.handleSelectionToolButton}>⬚</div>
           <div className="button" title="Undo" onClick={this.handleUndoButton}>↶</div>
